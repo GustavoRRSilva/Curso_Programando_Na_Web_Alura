470bfe7d5a8c60ebd6607a141a06a9e468027de7
GUSTAVO RODRIGUES RAMOS DA SILVA <01517429@sempreuninassau.com.br>
2022-08-22 08:43:14 -0300

    fim do modulo 8

---
+++ js/filtra.js
@@ -0,0 +1,30 @@
+var campoFiltro = document.querySelector("#filtrar-tabela");
+
+console.log(campoFiltro)
+campoFiltro.addEventListener("input",function(){
+    nomeFiltro=this.value;
+    console.log(nomeFiltro)
+    var pacientes = document.querySelectorAll(".paciente");
+    if(nomeFiltro.length>0){
+    pacientes.forEach(function(paciente){
+        var tdNome = paciente.querySelector(".info-nome")
+        var nome = tdNome.textContent;
+        var expressao = new RegExp(nomeFiltro,"i")
+        if(!expressao.test(nome)){
+            paciente.classList.add("invisivel")
+        }
+        else{
+                paciente.classList.remove("invisivel")
+        }
+    })
+    }
+    else{
+        pacientes.forEach(function(paciente){
+            paciente.classList.remove("invisivel")
+        });
+    }
+
+
+
+
+})
